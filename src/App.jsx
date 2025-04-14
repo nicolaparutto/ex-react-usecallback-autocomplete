937f2658f5d31d7dc7e3555643f706e03869b9cd
nicolaparutto <nicolaparutto2003@gmail.com>
2025-04-14 16:05:11 +0200

    feat(bonus): bonus completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useCallback } from "react"
 
 function App() {
 
   const [searched, setSearched] = useState("");
   const [products, setProducts] = useState([]);
+  const [product, setProduct] = useState();
 
+  // funzione di debouce generica
+  function debounce(callback, delay) {
+    let timer;
+    return (value) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        callback(value)
+      }, delay)
+    }
+  }
+
+  // funzione asincrona per la gestione dei prodotti risultato dalla ricerca nell'input
   const fetchProducts = async (searched) => {
-
     if (!searched.trim()) {
       setProducts([])
       return;
@@ -19,16 +31,34 @@ function App() {
       console.error(error)
     }
   }
+  // richiamo della funzione di debounce, con assegnazione tramite useCallback della funzione di callback + delay
+  const debounceFetchProducts = useCallback(
+    debounce(fetchProducts, 400)
+    , [])
 
+  // richiamo della funzione debounceFetchProducts, soltanto al cambiamento della dipendenza (searched)
   useEffect(() => {
-    fetchProducts(searched)
+    debounceFetchProducts(searched)
   }, [searched])
+
+  // funzione per la gestione della chiamata, per ricevere i dati relativi ad un singolo prodotto
+  async function fetchProduct(productId) {
+    setSearched("")
+    setProducts([])
+    try {
+      const response = await fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/products/${productId}`);
+      const productData = await response.json();
+      setProduct(productData)
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   return (
     <>
       <section className="container">
-        <div>
-          <h1>Trova un prodotto</h1>
+        <div className="search-box">
+          <h1 className="title">Trova un prodotto</h1>
           <input
             className="search-bar"
             placeholder="Cerca un prodotto..."
@@ -38,15 +68,35 @@ function App() {
           />
         </div>
         {products.length > 0 && (
-          <div className="searched-box">
+          <div className="searched-box ">
             <ul className="searched-list">
               {products.map((product) => (
-                <li key={product.id}>{product.name}</li>
+                <li key={product.id}>
+                  <a onClick={() => fetchProduct(product.id)}>
+                    <i class="fa-solid fa-magnifying-glass lens"></i>
+                    {product.name}
+                  </a>
+                </li>
               ))}
             </ul>
           </div>
         )}
       </section>
+
+      {product && (
+        <section className="container">
+          <div className="product-box">
+            <div className="product-img">
+              <img src={product.image} alt="" />
+            </div>
+            <div className="product-info">
+              <h1>{product.name}</h1>
+              <p>{product.description}</p>
+              <span>{product.price}€</span>
+            </div>
+          </div>
+        </section>
+      )}
     </>
   )
 }
