28740bf86321170c6bf7fdeffaf97f24eebc0c76
nicolaparutto <nicolaparutto2003@gmail.com>
2025-04-14 11:55:48 +0200

    feat: milestone 1 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,52 @@
+import { useState, useEffect } from "react"
+
 function App() {
+
+  const [searched, setSearched] = useState("");
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async (searched) => {
+
+    if (!searched.trim()) {
+      setProducts([])
+      return;
+    }
+    try {
+      const response = await fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/products?search=${searched}`);
+      const data = await response.json();
+      setProducts(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts(searched)
+  }, [searched])
+
   return (
     <>
-      <h1>Hello World!</h1>
+      <section className="container">
+        <div>
+          <h1>Trova un prodotto</h1>
+          <input
+            className="search-bar"
+            placeholder="Cerca un prodotto..."
+            type="text"
+            value={searched}
+            onChange={e => setSearched(e.target.value)}
+          />
+        </div>
+        {products.length > 0 && (
+          <div className="searched-box">
+            <ul className="searched-list">
+              {products.map((product) => (
+                <li key={product.id}>{product.name}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </section>
     </>
   )
 }
